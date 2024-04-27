94c201c74a70377c3891b5cfe49cb95614e1f573
kuerim <kuerim@taltech.ee>
2024-04-27 21:20:06 +0300

    add week1

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("Hello, world!");
+
+let firstName = 'Harry';
+let lastName = 'Potter';
+
+console.log(firstName);
+console.log("Hello," + firstName + "!")
+console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
